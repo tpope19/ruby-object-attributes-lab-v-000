b9be42f6f1101cbfa507603d91c13072877bd279
Thomas Pope <tpope0928@gmail.com>
2018-07-17 20:46:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,18 @@
 class Person
-  def initialize(name)
+
+  def name=(name)
     @name = name
   end
-
   def name
     @name
   end
-end
 
-person = Person.new("beyonce")
-puts person.name
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
