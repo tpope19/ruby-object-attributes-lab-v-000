ceec8a1115ec2ac7e80d506e897edc18b62e2186
Thomas Pope <tpope0928@gmail.com>
2018-07-17 20:28:25 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+     def name=(fido)
+       @dogs_name = fido
+     end
+
+     def name
+       @dogs_names
+     end
+
+     def breed=(beagle)
+        @breed = beagle
+     end
+
+     def breed
+       @breed
+    end
+end
